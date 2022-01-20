494a5442f9e8937d098b2fb366910c0e4a236621
Andrew <andrewmgunn26@gmail.com>
2022-01-20 14:38:04 -0600

    count and arrays playing

--- src/org/andrewgunn/Main.java
+++ src/org/andrewgunn/Main.java
@@ -1,10 +1,28 @@
 package org.andrewgunn;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
+
+    public static void countUp(int max) {
+
+        if (max > 10 || max < 0) {
+            System.err.println("Error");
+        } else {
+            for (int i = 0; i <= max; i++) {
+                System.out.printf("# %d\n", i);
+            }
+        }
+
+    }
+
+    public static void tryItOutCount() {
+
+    }
 
     public static void inputOutput() {
         Scanner keyboard = new Scanner(System.in);
@@ -23,8 +41,8 @@ public class Main {
         System.out.printf("You enterd %.2f", num);
     }
 
-    public static void main(String[] args) {
-        // write your code here
+
+    public static void switchStatements() {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter grade>");
         char grade = keyboard.nextLine().toUpperCase(Locale.ROOT).charAt(0);
@@ -37,6 +55,38 @@ public class Main {
             case 'D' -> System.out.println("1 pts");
             default -> System.out.println("0 pts");
         }
+    }
 
+
+    public static void arraysArrayList() {
+        Scanner keyboard = new Scanner(System.in);
+
+        String names[] = new String[5];
+        names[0] = "Brittney";
+        names[1] = "Stacy";
+        names[2] = "Matt";
+        names[3] = null;
+
+        for (String name : names) {
+            if (name == null) continue;
+            System.out.println(name);
+        }
+
+
+        List<String> nameList = new ArrayList<>();
+        nameList.add("Andy");
+        nameList.add("Gunn");
+        nameList.add("Odie");
+
+        for (String i : nameList) {
+            System.out.printf("%s", i);
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner keyboard = new Scanner(System.in);
+        int j = Integer.parseInt(keyboard.nextLine());
+        countUp(j);
     }
 }
