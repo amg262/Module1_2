fd5e1b6b90962d0500779fc7d5d7f1fe2e567cbc
Andrew <andrewmgunn26@gmail.com>
2022-01-20 13:57:15 -0600

    first examples

--- src/org/andrewgunn/Main.java
+++ src/org/andrewgunn/Main.java
@@ -1,8 +1,17 @@
 package org.andrewgunn;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter a word>");
+        String word = keyboard.nextLine();
+        System.out.printf("You entered %s", word);
+
     }
 }
