2a02ee7a8869fd78b6acbb5d8ea21caccde9ecbc
Andrew <andrewmgunn26@gmail.com>
2022-01-20 14:17:57 -0600

    working on switch statement

--- src/org/andrewgunn/Main.java
+++ src/org/andrewgunn/Main.java
@@ -1,17 +1,42 @@
 package org.andrewgunn;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
 
+    public static void inputOutput() {
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Enter a word>");
         String word = keyboard.nextLine();
-        System.out.printf("You entered %s", word);
+        System.out.printf("You entered %s\n", word);
+        System.out.print("Enter a #>");
+
+        while (!keyboard.hasNextDouble()) {
+            System.out.println("Enter valid number");
+            keyboard.nextLine();
+        }
+
+        double num = Double.parseDouble(keyboard.nextLine());
+        System.out.printf("You enterd %.2f", num);
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter grade>");
+        char grade = keyboard.nextLine().toUpperCase(Locale.ROOT).charAt(0);
+
+
+        switch (grade) {
+            case 'A' -> System.out.println("4 pts");
+            case 'B' -> System.out.println("3 pts");
+            case 'C' -> System.out.println("2 pts");
+            case 'D' -> System.out.println("1 pts");
+            default -> System.out.println("0 pts");
+        }
 
     }
 }
